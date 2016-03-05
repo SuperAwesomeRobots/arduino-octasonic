f9de8c12c1eb3b58165c9f192e2dd94daf37964f
Andy Grove <andygrove73@gmail.com>
2016-03-05 12:17:41 -0700

    improve test

--- examples/test/test.ino
+++ examples/test/test.ino
@@ -1,9 +1,10 @@
 #include <SPI.h>
 #include "octasonic.h"
 
+const int sensorCount = 1;
 const int chipSelectPin = 10;
 
-Octasonic octasonic(8, chipSelectPin);
+Octasonic octasonic(sensorCount, chipSelectPin);
 
 void setup() {
   Serial.begin(9600);
@@ -11,10 +12,11 @@ void setup() {
 
 void loop() {
   
-  for (int i=0; i<8; i++) {
+  for (int i=0; i<sensorCount; i++) {
     int j = octasonic.get(i);
     Serial.print(j);
     Serial.print("  ");
   }
   Serial.println();
+  delay(1000);
 }
