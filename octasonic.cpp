7529fda1cdbaf39fcbe95d5df500ed6178861c17
Andy Grove <andygrove73@gmail.com>
2016-03-09 21:01:00 -0700

    fix bugs

--- octasonic.cpp
+++ octasonic.cpp
@@ -29,14 +29,13 @@ void Octasonic::init(unsigned int num_sensors, unsigned int _chipSelect) {
 
 unsigned int Octasonic::get(unsigned int index) {
   send(0x30 | index);
-  return send(0x00);
+  unsigned int ret = send(0x00);
+  return ret;
 }
 
 /* set the interval between polling sensors in intervals of 10 ms up to a maximum of 15 x 10 ms */
 void Octasonic::set_poll_interval(unsigned int n) {
-  send(0x34 | n);
-  return send(0x00);
-
+  send(0x40 | n);
 }
 
 
--- octasonic.h
+++ octasonic.h
@@ -9,6 +9,9 @@ public:
   /* get the latest reading for a particular sensor */
   unsigned int get(unsigned int index);
 
+  /* set the interval between polling sensors in intervals of 10 ms up to a maximum of 15 x 10 ms */
+  void set_poll_interval(unsigned int n);
+
 private:
   void init(unsigned int num_sensors, unsigned int chipSelect);
   unsigned int send(unsigned int n);
